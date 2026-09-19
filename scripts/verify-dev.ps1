2d2154bf2056a2db9379b52bcc1bbfea970b4cf6
kaastumor <jeroen91@gmail.com>
2026-09-19 17:26:10 +0200

    scripts: include release reconstruction in verify-dev.ps1

--- scripts/verify-dev.ps1
+++ scripts/verify-dev.ps1
@@ -4,6 +4,7 @@ $ErrorActionPreference = "Stop"
 & .\scripts\db-test-schema.ps1
 & .\scripts\test-python.ps1
 & .\scripts\db-test-non-atlantic.ps1
+& .\scripts\db-test-release-reconstruction.ps1
 
 $workbook = "data/releases/v0.6.1/Historical_Slavery_Atlas_v0.6.1_Controlled_Atlantic_Ingestion.xlsx"
 if (Test-Path $workbook) {
