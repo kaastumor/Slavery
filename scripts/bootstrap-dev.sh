da5292592432dda44c7ee379fcaf4312b0036c97
kaastumor <jeroen91@gmail.com>
2026-09-19 17:26:06 +0200

    scripts: include release reconstruction in bootstrap-dev.sh

--- scripts/bootstrap-dev.sh
+++ scripts/bootstrap-dev.sh
@@ -17,6 +17,7 @@ docker compose up -d db
 ./scripts/db-test-schema.sh
 ./scripts/test-python.sh
 ./scripts/db-test-non-atlantic.sh
+./scripts/db-test-release-reconstruction.sh
 
 workbook="data/releases/v0.6.1/Historical_Slavery_Atlas_v0.6.1_Controlled_Atlantic_Ingestion.xlsx"
 if [[ -f "$workbook" ]]; then
