aebd9ce5127d9362a8dfa549be1100d6a2cdd77c
kaastumor <jeroen91@gmail.com>
2026-09-26 20:45:00 +0200

    Advance checksum-ledger inventory test to 0033

--- tests/test_bootstrap_migration_ledger.py
+++ tests/test_bootstrap_migration_ledger.py
@@ -13,11 +13,11 @@ from bootstrap_migration_ledger import (  # noqa: E402
 
 
 class BootstrapMigrationLedgerTests(unittest.TestCase):
-    def test_repository_inventory_is_exact_0001_to_0032(self):
+    def test_repository_inventory_is_exact_0001_to_0033(self):
         inventory = repository_inventory()
-        self.assertEqual(len(inventory), 32)
+        self.assertEqual(len(inventory), 33)
         self.assertEqual(inventory[0]["filename"], "0001_bootstrap.sql")
-        self.assertEqual(inventory[-1]["filename"], "0032_v3_source_relation_provenance.sql")
+        self.assertEqual(inventory[-1]["filename"], "0033_release_research_target_result_membership.sql")
         self.assertTrue(all(len(row["checksum_sha256"]) == 64 for row in inventory))
 
     def test_known_platform_history_irregularities_can_be_explicitly_accepted(self):
